e5625bd712450d581f09641c400451f33b0eab2e
Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>
2022-05-01 18:31:23 +0100

    Added update and added API keys to ignore

--- lib/services/api_key.dart
+++
@@ -1,7 +0,0 @@
-String getKey(){
-
-  return "AIzaSyCVWY6iv5AT-bRKkKgWLETNnOkRR2OQ-5Y";
-  //return "AIzaSyAJmuK8dfcvcmqTqqjDozITI_5bM0AJmvc"; ///Browser
-
-}
-//todo Git ignore, update this key and any further keys
